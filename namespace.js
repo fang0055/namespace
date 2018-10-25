f12c4af061b5d3252bdd0b0da3942c0b28407745
Link Fang <fang0055@algonquinlive.com>
2018-10-24 23:21:14 -0400

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -1,31 +1,27 @@
-var FANG0055 = {square function init();}
-document.addEventListener("DOMContentLoaded", init);
+var FANG0055 = {
+    init: function init() {
+        let square = document.createElement("div");
+        square.className = "box";
+        square.textContent = "fang0055";
+        let squares = document.getElementById("boxes");
+        squares.appendChild(square);
 
-function init() {
-    let square = document.createElement("div");
-    square.className = "box";
-    square.textContent = "fang0055";
-    let squares = document.getElementById("boxes");
-    squares.appendChild(square);
-   
-    // add some event listeners:
+        square.addEventListener("click", squareClick);
+        square.addEventListener("mouseover", mouseOver);
+        square.addEventListener("mouseout", mouseOut);
 
-    square.addEventListener("click", squareClick);
-    square.addEventListener("mouseover", mouseOver);
-    square.addEventListener("mouseout", mouseOut);
-}
+        function squareClick() {
+            this.style.borderColor = "hsl(182deg, 100%, 10%)";
+            this.style.backgroundColor = "hsl(182deg, 100%, 23%)";
+        }
 
-function squareClick() {
-    this.style.borderColor = "hsl(182deg, 100%, 10%)";
-    this.style.backgroundColor = "hsl(182deg, 100%, 23%)";
-}
+        function mouseOver(e) {
+            e.target.classList.toggle("highlight");
+        }
 
-function mouseOver(e) {
-    e.target.classList.toggle("highlight");
-}
-
-function mouseOut(e) {
-    e.target.classList.toggle("highlight");
-    e.target.removeAttribute('style'); // remove any style added by the click event
-}
-
+        function mouseOut(e) {
+            e.target.classList.toggle("highlight");
+            e.target.removeAttribute('style'); // remove any style added by the click event
+        }
+    }
+};
